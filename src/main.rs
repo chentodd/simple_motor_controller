3b46ca159f59f2a9ccd0daa59d5e661979b3e4c2
todd <chentodd2012@gmail.com>
2024-11-14 00:38:58 +0800

    chore: use `pwm` and `dir` to test left wheel motor

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,50 @@
 #![no_std]
 #![no_main]
 
-use core::u16;
-
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::peripherals;
-use embassy_stm32::timer::qei::*;
+use embassy_stm32::gpio::{Level, Output, OutputType, Speed};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::timer::low_level::OutputPolarity;
+use embassy_stm32::timer::qei::{Qei, QeiPin};
+use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    info!("Start encoder test..");
-
     let p = embassy_stm32::init(Default::default());
 
-    // Test left wheel encoder
-    let enc_a: QeiPin<'_, peripherals::TIM2, Ch1> = QeiPin::new_ch1(p.PA0);
-    let enc_b: QeiPin<'_, peripherals::TIM2, Ch2> = QeiPin::new_ch2(p.PA1);
+    // left wheel encoder
+    let enc_a = QeiPin::new_ch1(p.PA0);
+    let enc_b = QeiPin::new_ch2(p.PA1);
     let qei_enc = Qei::new(p.TIM2, enc_a, enc_b);
+
+    // left wheel pwm
+    let pwm_ch3_pin = PwmPin::new_ch3(p.PB0, OutputType::PushPull);
+    let mut pwm = SimplePwm::new(
+        p.TIM3, 
+        None, 
+        None, 
+        Some(pwm_ch3_pin), 
+        None, 
+        Hertz::khz(20), 
+        Default::default());
+
+    let mut pwm_ch3 = pwm.ch3();
+    pwm_ch3.set_polarity(OutputPolarity::ActiveLow);
+    pwm_ch3.enable();
+
+    // left wheel direction pin
+    // HIGH: CCW
+    // LOW: CW
+    let mut dir_pin = Output::new(p.PA4, Level::High, Speed::Low);
     
+    // Test
     let mut enc_count: i32 = 0;
     let mut prev_qei_count: i32 = 0;
+
+    pwm_ch3.set_duty_cycle_percent(10);
     loop {
         let curr_qei_count = (qei_enc.count() as i16) as i32;
         let diff = curr_qei_count.wrapping_sub(prev_qei_count);
