4ef93a8571704b2bdda90ce9c0d01f30a4ba9b12
todd <chentodd2012@gmail.com>
2024-11-18 23:12:01 +0800

    refactor: update file heavily to test motor pid velocity control feature
    Here are the steps that put things together to test pid velocity control:
    1. Prepare a timer interrupt. In this case, `TIM4` is used, and it will trigger `run` task with 200Hz(run in 5ms)
    2. The `run` task will wait for the signal from timer interrupt, and run velocity control loop after receiving signal
    
    Note:
    - The target velocity is hard-coded, and I'll try to improve it by sending target velocity using serial communication
    - I'm not sure this is the ideal way to trigger the task periodically. I tried to use InterruptExecutor to trigger the task but it seemed not working on
    myside (I'm a rookie on embassy, so maybe I get somthing wrong). However, this method works now, and I may improve it when I get familiar with
    embassy

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,114 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use nucleo_f401re_rust::encoder::Encoder;
+use nucleo_f401re_rust::motor::BldcMotor24H;
+use nucleo_f401re_rust::pid::Pid;
+
 use embassy_executor::Spawner;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::signal::Signal;
+use embassy_time::Timer;
+
 use embassy_stm32::gpio::{Level, Output, OutputType, Speed};
+use embassy_stm32::interrupt;
+use embassy_stm32::pac;
+use embassy_stm32::peripherals::{TIM2, TIM3, TIM4};
 use embassy_stm32::time::Hertz;
-use embassy_stm32::timer::low_level::OutputPolarity;
-use embassy_stm32::timer::qei::{Qei, QeiPin};
+use embassy_stm32::timer::low_level::{CountingMode, Timer as LLTimer};
 use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
-use embassy_time::{Duration, Timer};
+
 use {defmt_rtt as _, panic_probe as _};
 
+const PERIOD_S: f32 = 0.005;
+static TIMER_SIGNAL: Signal<CriticalSectionRawMutex, ()> = Signal::new();
+
+#[interrupt]
+unsafe fn TIM5() {
+    // Trigger the signal to notify the task
+    TIMER_SIGNAL.signal(());
+    pac::TIM5.sr().modify(|r| r.set_uif(false));
+}
+
+#[embassy_executor::task]
+async fn run(
+    mut left_wheel: BldcMotor24H<'static, TIM2, TIM3>,
+    mut right_wheel: BldcMotor24H<'static, TIM4, TIM3>,
+) {
+    // left_wheel.set_target_velocity(1000.0);
+    // right_wheel.set_target_velocity(-1000.0);
+
+    loop {
+        TIMER_SIGNAL.wait().await;
+        left_wheel.run_pid_velocity_control();
+        right_wheel.run_pid_velocity_control();
+    }
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
+    // Init
     let p = embassy_stm32::init(Default::default());
 
-    // left wheel encoder
-    let enc_a = QeiPin::new_ch1(p.PA0);
-    let enc_b = QeiPin::new_ch2(p.PA1);
-    let qei_enc = Qei::new(p.TIM2, enc_a, enc_b);
+    let left_wheel_enc: Encoder<'_, TIM2, 400> = Encoder::new(p.TIM2, p.PA0, p.PA1);
+    let left_wheel_pwm_pin = PwmPin::new_ch3(p.PB0, OutputType::PushPull);
+    let left_wheel_dir_pin = Output::new(p.PA4, Level::High, Speed::Low);
+    let left_wheel_break_pin = Output::new(p.PC1, Level::High, Speed::Low);
+    let left_wheel_pid = Pid::new(0.00006, 0.00124, 0.000000728, 1.0);
 
-    // left wheel pwm
-    let pwm_ch3_pin = PwmPin::new_ch3(p.PB0, OutputType::PushPull);
-    let mut pwm = SimplePwm::new(
-        p.TIM3, 
-        None, 
-        None, 
-        Some(pwm_ch3_pin), 
-        None, 
-        Hertz::khz(20), 
-        Default::default());
+    let right_wheel_enc: Encoder<'_, TIM4, 400> = Encoder::new(p.TIM4, p.PB6, p.PB7);
+    let right_wheel_pwm_pin = PwmPin::new_ch1(p.PB4, OutputType::PushPull);
+    let right_wheel_dir_pin = Output::new(p.PB5, Level::High, Speed::Low);
+    let right_wheel_break_pin = Output::new(p.PB3, Level::High, Speed::Low);
+    let right_wheel_pid = Pid::new(0.00006, 0.00124, 0.000000728, 1.0);
 
-    let mut pwm_ch3 = pwm.ch3();
-    pwm_ch3.set_polarity(OutputPolarity::ActiveLow);
-    pwm_ch3.enable();
+    let pwm = SimplePwm::new(
+        p.TIM3,
+        Some(right_wheel_pwm_pin),
+        None,
+        Some(left_wheel_pwm_pin),
+        None,
+        Hertz::khz(20),
+        Default::default(),
+    );
 
-    // left wheel direction pin
-    // HIGH: CCW
-    // LOW: CW
-    let mut dir_pin = Output::new(p.PA4, Level::High, Speed::Low);
-    
+    let pwm_channels = pwm.split();
+    let left_wheel_pwm_ch = pwm_channels.ch3;
+    let right_wheel_pwm_ch = pwm_channels.ch1;
+
+    // Create wheels
+    let left_wheel = BldcMotor24H::new(
+        left_wheel_enc,
+        left_wheel_pwm_ch,
+        left_wheel_dir_pin,
+        left_wheel_break_pin,
+        left_wheel_pid,
+        PERIOD_S,
+    );
+
+    let right_wheel = BldcMotor24H::new(
+        right_wheel_enc,
+        right_wheel_pwm_ch,
+        right_wheel_dir_pin,
+        right_wheel_break_pin,
+        right_wheel_pid,
+        PERIOD_S,
+    );
+
+    // Create timer
+    let low_level_timer = LLTimer::new(p.TIM5);
+    low_level_timer.set_counting_mode(CountingMode::EdgeAlignedUp);
+    low_level_timer.set_frequency(Hertz::hz(200));
+    low_level_timer.set_autoreload_preload(true);
+    low_level_timer.enable_update_interrupt(true);
+    low_level_timer.start();
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(interrupt::TIM5);
+    }
+
     // Test
-    let mut enc_count: i32 = 0;
-    let mut prev_qei_count: i32 = 0;
-
-    pwm_ch3.set_duty_cycle_percent(10);
+    spawner.spawn(run(left_wheel, right_wheel)).unwrap();
     loop {
-        let curr_qei_count = (qei_enc.count() as i16) as i32;
-        let diff = curr_qei_count.wrapping_sub(prev_qei_count);
-        enc_count += diff;
-        
-        println!("{}, {}, {}, {}", enc_count, diff, curr_qei_count, prev_qei_count);
-        prev_qei_count = curr_qei_count;
-        Timer::after(Duration::from_millis(10)).await;
+        Timer::after_secs(1).await;
     }
 }
