477e25a16e55c77e0bce7fd74cb9751825278571
todd <chentodd2012@gmail.com>
2024-11-29 22:05:03 +0800

    chore: build `proto` file

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
+use micropb_gen::Generator;
+
 fn main() {
+    let mut gen = Generator::new();
+    gen.use_container_heapless()
+        .add_protoc_arg("-Iproto")
+        .compile_protos(&["command.proto"], "src/proto_packet.rs")
+        .unwrap();
+
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
