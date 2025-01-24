34a5aa9071f6133c95a83a2414e62e3da61b8880
todd <chentodd2012@gmail.com>
2025-01-24 22:54:32 +0800

    chore: simple test on `ConnectionWindow`

--- serial_tool/src/bin/app.rs
+++ serial_tool/src/bin/app.rs
@@ -1,39 +1,4 @@
-use eframe::{egui, App};
-use egui::ComboBox;
-
-#[derive(Default)]
-struct ConnectionWindow {
-    port: &'static str,
-}
-
-impl ConnectionWindow {
-    const PORTS: [&'static str; 2] = ["/dev/ttyUSB0", "/dev/ttyACM0"];
-}
-
-impl ConnectionWindow {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self {
-            port: Self::PORTS[0],
-        }
-    }
-}
-
-impl App for ConnectionWindow {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { port } = self;
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Test");
-            ComboBox::new("port", "Port")
-                .selected_text(*port)
-                .show_ui(ui, |ui| {
-                    for x in Self::PORTS {
-                        ui.selectable_value(port, x, x);
-                    }
-                });
-        });
-    }
-}
+use serial_tool::main_window::ConnectionWindow;
 
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions::default();
