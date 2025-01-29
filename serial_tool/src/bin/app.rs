722f0a5513c83d6ebe18bc21a91fe90548dec992
todd <chentodd2012@gmail.com>
2025-01-29 22:52:07 +0800

    chore: add connection panel  and mode panel and do simple manual test
    I follow the suggestion in `eframe` template and put the panels
    into `MainWindow` struct.
    
    Note: only UIs are available, the core logic isn't ready

--- serial_tool/src/bin/app.rs
+++ serial_tool/src/bin/app.rs
@@ -1,10 +1,10 @@
-use serial_tool::main_window::ConnectionWindow;
+use serial_tool::main_window::*;
 
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "MyApp",
         native_options,
-        Box::new(|cc| Ok(Box::new(ConnectionWindow::new(cc)))),
+        Box::new(|cc| Ok(Box::new(MainWindow::new(cc)))),
     )
 }
--- serial_tool/src/main_window.rs
+++ serial_tool/src/main_window.rs
@@ -1,40 +1,106 @@
-use eframe::{egui, App, CreationContext};
-use egui::{Ui, ComboBox};
+use std::fmt::Display;
+
+use eframe::{
+    egui::{self, Button, Vec2}, App, CreationContext
+};
+use egui::{ComboBox, Ui};
 
 use serial_enumerator::{get_serial_list, SerialInfo};
 
-pub struct ConnectionWindow {
+use crate::proto::motor_::Operation;
+
+#[derive(Default)]
+struct ConnectionSettings {
     serial_ports: Vec<SerialInfo>,
     selected_port: String,
+    button_clicked: bool,
 }
 
-impl App for ConnectionWindow {
+#[derive(Default)]
+pub struct MainWindow {
+    conn_settings: ConnectionSettings,
+    selected_mode: Operation,
+}
+
+impl Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match Operation::from(self.0) {
+            Operation::Unspecified => write!(f, "Unspecified"),
+            Operation::IntpPos => write!(f, "IntpPos"),
+            Operation::IntpVel => write!(f, "IntpVel"),
+            _ => Ok(()),
+        }
+    }
+}
+
+impl App for MainWindow {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Connection");
-            self.display(ui);
+        egui::TopBottomPanel::top("funtionality_panel").show(ctx, |ui| {
+            ui.columns(2, |col| {
+                col[0].allocate_ui(Vec2::new(0.0, 0.0), |ui| {
+                    ui.heading("Connection setup");
+                    self.display_connection_panel(ui);
+                });
+
+                col[1].allocate_ui(Vec2::new(0.0, 0.0), |ui| {
+                    ui.heading("Mode setup");
+                    self.display_mode_panel(ui);
+                });
+            });
         });
     }
 }
 
-impl ConnectionWindow {
+impl MainWindow {
     pub fn new(_cc: &CreationContext<'_>) -> Self {
         Self {
-            serial_ports: get_serial_list(),
-            selected_port: "".to_owned(),
+            conn_settings: ConnectionSettings {
+                serial_ports: get_serial_list(),
+                ..Default::default()
+            },
+            ..Default::default()
         }
     }
 
-    pub fn display(&mut self, ui: &mut Ui) {
-        let curr_selected = &mut self.selected_port.as_str();
-        ComboBox::new("ports", "ports")
-            .selected_text(*curr_selected)
+    fn display_connection_panel(&mut self, ui: &mut Ui) {
+        let ConnectionSettings {
+            serial_ports,
+            selected_port,
+            button_clicked,
+        } = &mut self.conn_settings;
+
+        ui.horizontal_centered(|ui| {
+            let curr_selected = &mut selected_port.as_str();
+            ComboBox::new("ports", "ports")
+                .selected_text(*curr_selected)
+                .show_ui(ui, |ui| {
+                    for port in serial_ports.iter() {
+                        ui.selectable_value(curr_selected, &port.name, &port.name);
+                    }
+                });
+            *selected_port = curr_selected.to_owned();
+
+            let text_in_button = if *button_clicked { "Stop" } else { "Start" };
+            let conn_button = Button::new(text_in_button);
+            if ui.add_enabled(!selected_port.is_empty(), conn_button).clicked() {
+                *button_clicked = !*button_clicked;
+            }
+        });
+    }
+
+    fn display_mode_panel(&mut self, ui: &mut Ui) {
+        if !self.conn_settings.button_clicked {
+            ui.disable();
+        }
+        let curr_selected = &mut self.selected_mode;
+        ComboBox::new("control_mods", "control modes")
+            .selected_text(format!("{}", curr_selected))
             .show_ui(ui, |ui| {
-                for port in self.serial_ports.iter() {
-                    ui.selectable_value(curr_selected, &port.name, &port.name);
-                }
+                ui.selectable_value(curr_selected, Operation::Unspecified, "Unspecified");
+                ui.selectable_value(curr_selected, Operation::IntpPos, "IntpPos");
+                ui.selectable_value(curr_selected, Operation::IntpVel, "IntpVel");
             });
-        
-        self.selected_port = curr_selected.to_owned();
+
+        self.selected_mode = *curr_selected;
     }
 }
