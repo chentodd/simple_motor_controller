5929708caa8aad25e0dfce5a54e5532991e89c17
todd <chentodd2012@gmail.com>
2025-02-07 23:47:59 +0800

    chore: create `Receiver` to satisfy `MainWindow` for simple  test

--- serial_tool/src/bin/app.rs
+++ serial_tool/src/bin/app.rs
@@ -1,10 +1,15 @@
+use std::sync::mpsc::channel;
+
 use serial_tool::main_window::*;
+use serial_tool::profile_measurement::ProfileData;
 
 fn main() -> eframe::Result {
+    let (_tx, rx) = channel::<ProfileData>();
+
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "MyApp",
         native_options,
-        Box::new(|cc| Ok(Box::new(MainWindow::new(cc, 4000)))),
+        Box::new(|cc| Ok(Box::new(MainWindow::new(cc, 4000, rx)))),
     )
 }
