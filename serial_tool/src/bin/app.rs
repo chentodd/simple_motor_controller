9ea6f0a7179d8a9ee51046922d270bfae3023afb
todd <chentodd2012@gmail.com>
2025-02-22 23:53:57 +0800

    chore: init `env_logger`

--- serial_tool/src/bin/app.rs
+++ serial_tool/src/bin/app.rs
@@ -1,6 +1,8 @@
 use serial_tool::main_window::*;
 
 fn main() -> eframe::Result {
+    env_logger::init();
+
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "MyApp",
