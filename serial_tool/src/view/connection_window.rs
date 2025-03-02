50a1a6adcb213e863aa0b74ffd6ba49a400a52b0
todd <chentodd2012@gmail.com>
2025-03-03 01:20:52 +0800

    chore: move `connection_window` to different module

---
+++ serial_tool/src/view/connection_window.rs
@@ -0,0 +1,61 @@
+use crate::{UiView, ViewEvent, ViewRequest};
+use eframe::egui::{Button, ComboBox, Ui};
+use serial_enumerator::get_serial_list;
+
+#[derive(Default)]
+pub(super) struct ConnectionWindow {
+    selected_port: String,
+    start: bool,
+}
+
+impl ConnectionWindow {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+}
+
+impl UiView for ConnectionWindow {
+    fn show(&mut self, ui: &mut Ui) {
+        let port_names = get_serial_list()
+            .iter()
+            .map(|x| x.name.clone())
+            .collect::<Vec<String>>();
+
+        ui.heading("Connection setup");
+        ui.horizontal_centered(|ui| {
+            let curr_selected = &mut self.selected_port.as_str();
+            ComboBox::new("ports", "ports")
+                .selected_text(*curr_selected)
+                .show_ui(ui, |ui| {
+                    for port in &port_names {
+                        ui.selectable_value(curr_selected, port, port);
+                    }
+                });
+            self.selected_port = curr_selected.to_owned();
+
+            let text_in_button = if self.start { "Stop" } else { "Start" };
+
+            let conn_button = Button::new(text_in_button);
+            if ui
+                .add_enabled(!self.selected_port.is_empty(), conn_button)
+                .clicked()
+            {
+                self.start = !self.start;
+            }
+        });
+    }
+
+    fn take_request(&mut self) -> Option<ViewRequest> {
+        if self.start {
+            Some(ViewRequest::Connection(self.start, self.selected_port.clone()))
+        } else {
+            None
+        }
+    }
+
+    fn handle_event(&mut self, _event: ViewEvent) {
+        // Do nothing
+    }
+}
