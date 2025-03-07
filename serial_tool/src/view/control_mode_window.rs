e8c399c730c628823e2b4ab98adfefbbbd5e9733
todd <chentodd2012@gmail.com>
2025-03-08 00:24:01 +0800

    chore: add `control_mode_window`
    This window is responsible for:
    1. Receive target mode request from user
    2. Receive internal target mode request from `main_window`
    
    and it will display progress bar if `target_mode != curr_mode`

---
+++ serial_tool/src/view/control_mode_window.rs
@@ -0,0 +1,96 @@
+use crate::{proto::motor_::Operation, UiView, ViewEvent, ViewRequest};
+use eframe::egui::{self, ComboBox, Id, ProgressBar, Ui, Widget};
+
+#[derive(Default)]
+pub(super) struct ControlModeWindow {
+    curr_control_mode: Operation,
+    target_control_mode: Operation,
+    internal_request: (Operation, String),
+    mode_switch_progress: Option<f32>,
+}
+
+impl ControlModeWindow {
+    pub fn new() -> Self {
+        Self {
+            curr_control_mode: Operation::IntpVel,
+            target_control_mode: Operation::IntpVel,
+            ..Default::default()
+        }
+    }
+}
+
+impl UiView for ControlModeWindow {
+    fn show(&mut self, ui: &mut Ui) {
+        ui.heading("Control mode setup");
+        ComboBox::new("control_modes", "control_modes")
+            .selected_text(format!("{}", self.target_control_mode))
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut self.target_control_mode, Operation::IntpPos, "IntpPos");
+                ui.selectable_value(&mut self.target_control_mode, Operation::IntpVel, "IntpVel");
+            });
+
+        // Target mode is requested by internal functions
+        let (mode, title) = &self.internal_request;
+        let modal_title = if *mode != Operation::Unspecified {
+            self.target_control_mode = *mode;
+            title
+        } else {
+            "Switch control mode"
+        };
+
+        if self.target_control_mode != self.curr_control_mode {
+            egui::Modal::new(Id::new(modal_title)).show(ui.ctx(), |ui| {
+                ui.heading("Are you sure?");
+                egui::Sides::new().show(
+                    ui,
+                    |_ui| {},
+                    |ui| {
+                        if ui.button("Yes").clicked() {
+                            self.mode_switch_progress = Some(0.0);
+                        }
+
+                        if ui.button("No").clicked() {
+                            self.mode_switch_progress = None;
+                            self.target_control_mode = self.curr_control_mode;
+                        }
+                    },
+                );
+            });
+        }
+
+        if let Some(progress) = self.mode_switch_progress {
+            egui::Modal::new(Id::new("Switch progress")).show(ui.ctx(), |ui| {
+                ui.heading("Switching");
+
+                ProgressBar::new(progress).ui(ui);
+
+                if self.target_control_mode != self.curr_control_mode {
+                    self.mode_switch_progress = Some(progress + 0.001);
+                    ui.ctx().request_repaint();
+                } else {
+                    self.mode_switch_progress = None;
+                }
+            });
+        }
+    }
+
+    fn take_request(&mut self) -> Option<ViewRequest> {
+        if self.target_control_mode != self.curr_control_mode {
+            Some(ViewRequest::ModeSwitch(self.target_control_mode))
+        } else {
+            None
+        }
+    }
+
+    fn handle_event(&mut self, event: ViewEvent) {
+        match event {
+            ViewEvent::ControlModeUpdate(mode) if mode != Operation::Stop => {
+                self.curr_control_mode = mode
+            }
+            ViewEvent::InternalControlModeRequest(x) => {
+                self.internal_request = x;
+            }
+            _ => (),
+        }
+    }
+}
