5e3d128ab84e451c4fdb192b07f82c7ce357d33e
todd <chentodd2012@gmail.com>
2025-03-23 22:50:52 +0800

    chore: set intp status to busy in `stop`
    The interpolation will not run if it is already finished, so we stop
    axis after interpolation is finished, we need to set the state to
    busy to ignite interpolation.

--- s_curve/src/lib.rs
+++ s_curve/src/lib.rs
@@ -171,8 +171,9 @@ impl SCurveInterpolator {
     }
 
     pub fn stop(&mut self) {
-        self.target_data.vel_end = 0.0;
         self.target_data.dist = 0.0;
+        self.target_data.vel_end = 0.0;
+        self.intp_status = InterpolationStatus::Busy;
     }
 
     fn calculate_dec_distance(&mut self) {
