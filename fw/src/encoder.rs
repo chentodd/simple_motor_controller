8a4cced44aee7541cdb179a5e75ad0d084693b53
todd <chentodd2012@gmail.com>
2025-04-02 00:19:14 +0800

    chore: use original velocity calculation method and fix errors in actual position calculation

--- fw/src/encoder.rs
+++ fw/src/encoder.rs
@@ -1,14 +1,16 @@
+use core::f32::consts::PI;
+
 use embassy_stm32::timer::qei::*;
 use embassy_stm32::timer::GeneralInstance4Channel;
 use embassy_stm32::timer::{Channel1Pin, Channel2Pin};
 use embassy_stm32::Peripheral;
-use heapless::Vec;
 
 pub struct Encoder<'a, T: GeneralInstance4Channel, const COUNTS_PER_REV: u16> {
     qei: Qei<'a, T>,
     act_vel: f32,
-    f_values: Vec<f32, 3>,
+    act_pos: f32,
     curr_enc_count: i32,
+    prev_enc_count: i32,
     prev_qei_count: i16,
     curr_qei_count: i16,
 }
@@ -24,8 +26,9 @@ impl<'a, T: GeneralInstance4Channel, const COUNTS_PER_REV: u16> Encoder<'a, T, C
         Self {
             qei: Qei::new(tim, enc_a_pin, enc_b_pin),
             act_vel: 0.0,
-            f_values: Vec::new(),
+            act_pos: 0.0,
             curr_enc_count: 0,
+            prev_enc_count: 0,
             prev_qei_count: 0,
             curr_qei_count: 0,
         }
@@ -36,7 +39,7 @@ impl<'a, T: GeneralInstance4Channel, const COUNTS_PER_REV: u16> Encoder<'a, T, C
     }
 
     pub fn get_act_position_in_rad(&self) -> f32 {
-        (self.curr_enc_count as f32) / (COUNTS_PER_REV as f32)
+        self.act_pos
     }
 
     pub fn get_act_velocity_in_rpm(&self) -> f32 {
@@ -46,24 +49,12 @@ impl<'a, T: GeneralInstance4Channel, const COUNTS_PER_REV: u16> Encoder<'a, T, C
     pub fn update_act_velocity_in_rpm(&mut self, period_s: f32) {
         self.update_encoder_count();
 
-        let prev_count = self.f_values.last().cloned().unwrap_or(0.0);
-        let curr_count = self.curr_enc_count as f32;
-        let filtered_enc = 0.8 * prev_count + 0.2 * curr_count;
-        let vel_count_s = if !self.f_values.is_full() {
-            let _ = self.f_values.push(filtered_enc);
-            (filtered_enc - prev_count) as f32 / period_s
-        } else {
-            let n = self.f_values.len();
-            self.f_values.rotate_right(n - 1);
-            self.f_values[n - 1] = filtered_enc;
+        let diff_count: f32 = (self.curr_enc_count - self.prev_enc_count) as f32;
+        let round_s = diff_count / period_s / (COUNTS_PER_REV as f32);
+        self.act_vel = 60.0 * round_s;
+        self.act_pos += 2.0 * PI * round_s * period_s;
 
-            let f0 = &self.f_values[n - 1];
-            let f1 = &self.f_values[n - 2];
-            let f2 = &self.f_values[n - 3];
-            (3.0 * f0 - 4.0 * f1 + f2) as f32 / (2.0 * period_s)
-        };
-
-        self.act_vel = 60.0 * vel_count_s / (COUNTS_PER_REV as f32);
+        self.prev_enc_count = self.curr_enc_count;
     }
 
     fn update_encoder_count(&mut self) {
