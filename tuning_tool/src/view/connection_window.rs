dd3087efba3e9b2c74d1f95efddb68f92c6487ba
todd <chentodd2012@gmail.com>
2025-04-27 22:23:54 +0800

    refactor!: update `ConnectionWindow` with `nusb` to scan connected devices

--- tuning_tool/src/view/connection_window.rs
+++ tuning_tool/src/view/connection_window.rs
@@ -1,10 +1,40 @@
-use crate::{UiView, ViewEvent, ViewRequest};
+use std::fmt::Display;
+
 use eframe::egui::{Button, ComboBox, Ui};
-use serial_enumerator::get_serial_list;
+use nusb;
+
+use crate::{UiView, ViewEvent, ViewRequest};
+
+#[derive(Default, PartialEq)]
+struct UsbInfo {
+    vendor_id: u16,
+    product_id: u16,
+    product_string: String,
+}
+
+impl Display for UsbInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:#06x}, {:#06x}, {}",
+            self.vendor_id, self.product_id, self.product_string
+        )
+    }
+}
+
+impl From<nusb::DeviceInfo> for UsbInfo {
+    fn from(device: nusb::DeviceInfo) -> Self {
+        Self {
+            vendor_id: device.vendor_id(),
+            product_id: device.product_id(),
+            product_string: device.product_string().unwrap_or_default().to_string(),
+        }
+    }
+}
 
 #[derive(Default)]
 pub(super) struct ConnectionWindow {
-    selected_port: String,
+    selected_usb: UsbInfo,
     target: bool,
     curr: bool,
     request: Option<ViewRequest>,
@@ -20,33 +50,32 @@ impl ConnectionWindow {
 
 impl UiView for ConnectionWindow {
     fn show(&mut self, ui: &mut Ui) {
-        let port_names = get_serial_list()
-            .iter()
-            .map(|x| x.name.clone())
-            .collect::<Vec<String>>();
+        let devices = nusb::list_devices().unwrap();
 
         ui.heading("Connection setup");
         ui.horizontal_centered(|ui| {
-            let curr_selected = &mut self.selected_port.as_str();
-            ComboBox::new("ports", "ports")
-                .selected_text(*curr_selected)
+            ComboBox::new("usb", "usbs")
+                .selected_text(format!("{}", self.selected_usb))
                 .show_ui(ui, |ui| {
-                    for port in &port_names {
-                        ui.selectable_value(curr_selected, port, port);
+                    for device in devices {
+                        let usb_info = UsbInfo::from(device);
+                        let text = usb_info.to_string();
+                        ui.selectable_value(&mut self.selected_usb, usb_info, text);
                     }
                 });
-            self.selected_port = curr_selected.to_owned();
 
             let text_in_button = if self.curr { "Stop" } else { "Start" };
             let conn_button = Button::new(text_in_button);
 
             if ui
-                .add_enabled(!self.selected_port.is_empty(), conn_button)
+                .add_enabled(!self.selected_usb.product_string.is_empty(), conn_button)
                 .clicked()
             {
                 self.target = !self.curr;
                 if self.target {
-                    self.request = Some(ViewRequest::ConnectionStart(self.selected_port.clone()));
+                    self.request = Some(ViewRequest::ConnectionStart(
+                        self.selected_usb.product_string.clone(),
+                    ));
                 } else {
                     self.request = Some(ViewRequest::ConnectionStop);
                 }
